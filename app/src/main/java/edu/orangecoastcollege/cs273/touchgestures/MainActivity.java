7cf064b8243db5b6f1a0610dbe23d88b0382b856
sbadajoz <sbadajoz@student.cccd.edu>
2017-11-02 15:21:59 -0700

    Edit onCreate, Implement: GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener

--- app/src/main/java/edu/orangecoastcollege/cs273/touchgestures/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/touchgestures/MainActivity.java
@@ -2,10 +2,12 @@ package edu.orangecoastcollege.cs273.touchgestures;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     private ScrollView gesturesScrollView;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView scrollTextView;
     private TextView flingTextView;
 
+    private GestureDetector mGestureDetector;
+
     private int singleTaps = 0, doubleTaps = 0, longPresses = 0, scrolls = 0, flings = 0;
 
     @Override
@@ -23,7 +27,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gesturesScrollView = (ScrollView) findViewById(R.id.gesturesScrollView);
+
+        gesturesLogTextView = (TextView) findViewById(R.id.gesturesLogTextView);
+        singleTapTextView = (TextView) findViewById(R.id.singleTapTextView);
+        doubleTapTextView = (TextView) findViewById(R.id.doubleTapTextView);
+        longPressTextView = (TextView) findViewById(R.id.longPressTextView);
+        gesturesLogTextView = (TextView) findViewById(R.id.gesturesLogTextView);
+        flingTextView = (TextView) findViewById(R.id.flingTextView);
+
+        mGestureDetector = new GestureDetector(gesturesScrollView.getContext(), this);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
 
     }
 
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
 }
